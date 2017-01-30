b1c72f61b312eea72858a67b36c0919e329e9110
Suyash <dextrous93@gmail.com>
2017-01-30 19:49:36 +0530

    circular_queue_test: add more assertions for size

--- circular_queue_test.cc
+++ circular_queue_test.cc
@@ -5,16 +5,33 @@
 TEST(circular_queue, basic) {
 	chan::circular_queue<int> queue(2);
 
+	ASSERT_EQ(true, queue.empty());
+	ASSERT_EQ(false, queue.full());
+
 	queue.push(1);
 	queue.push(2);
+
+	ASSERT_EQ(false, queue.empty());
+	ASSERT_EQ(true, queue.full());
 
 	ASSERT_EQ(1, queue.front());
 	queue.pop();
 
+	ASSERT_EQ(false, queue.empty());
+	ASSERT_EQ(false, queue.full());
+
+	ASSERT_EQ(1, queue.size());
+
 	queue.push(3);
+
+	ASSERT_EQ(false, queue.empty());
+	ASSERT_EQ(true, queue.full());
 
 	ASSERT_EQ(2, queue.front());
 	queue.pop();
 	ASSERT_EQ(3, queue.front());
 	queue.pop();
+
+	ASSERT_EQ(true, queue.empty());
+	ASSERT_EQ(false, queue.full());
 }
