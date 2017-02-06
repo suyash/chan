2f916fc9d6ae15d3599c97225df09d4741748d7a
Suyash <dextrous93@gmail.com>
2017-02-06 20:58:39 +0530

    examples/filter: fix segfault

--- examples/filter.cc
+++ examples/filter.cc
@@ -4,7 +4,7 @@
 #include "../chan.hh"
 
 void producer(chan::write_chan<int>& ch) {
-	for (int i = 1 ; ; i++) {
+	for (int i = 1 ; i <= 30 ; i++) {
 		ch << i;
 	}
 }
@@ -14,7 +14,7 @@ void filter(
 	chan::write_chan<int>& out,
 	int f
 ) {
-	for (int i = 0 ; ; i++) {
+	for (int i = 0 ; i < 30 ; i++) {
 		int x = 0;
 		in >> x;
 
@@ -26,12 +26,10 @@ void filter(
 
 int main() {
 	chan::unbuffered_chan<int> ch;
-	std::thread t(producer, std::ref(ch));
-	t.detach();
+	std::thread(producer, std::ref(ch)).detach();
 
 	chan::unbuffered_chan<int> ch2;
-	std::thread t2(filter, std::ref(ch), std::ref(ch2), 3);
-	t2.detach();
+	std::thread(filter, std::ref(ch), std::ref(ch2), 3).detach();
 
 	for (int i = 0 ; i < 10 ; i++) {
 		int x = 0;
